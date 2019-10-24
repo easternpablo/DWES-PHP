576c8e58dacec00db34cc4e816d4635e870862d1
easternpablo <easternpablo@gmail.com>
2019-10-25 00:00:22 +0200

    commit
    Añadir ejercicios

--- Unidad 2/Actividades/pdf/Includes y páginas reiteradas/Ejercicio2/cajafuerte.php
+++ Unidad 2/Actividades/pdf/Includes y páginas reiteradas/Ejercicio2/cajafuerte.php
@@ -7,6 +7,20 @@
     <body>
         <?php
             
+            $numeroPensado = rand(1,9999);        
+            if(!isset($_POST['campoNumSecreto'])){               
+                $numIntroducido = 0;
+            }else{
+                $numIntroducido = $_POST['campoNumSecreto'];
+                if($numIntroducido == $numeroPensado){
+                    echo "<h1>Enhorabuena has acertado el número!!!</h1>";
+                }elseif($numIntroducido != $numeroPensado){
+                    echo "<h1>Ese no es el número sigue intentándolo</h1>";
+                }
+            }
+            
+            require_once 'index.php';
+        
         ?>
     </body>
 </html>
--- Unidad 2/Actividades/pdf/Includes y páginas reiteradas/Ejercicio2/index.php
+++ Unidad 2/Actividades/pdf/Includes y páginas reiteradas/Ejercicio2/index.php
@@ -8,8 +8,8 @@
         <h1>Ejercicio 2</h2>
         <p>Intente adivinar la combinación de la caja fuerte (4 dígitos)</p>
         <form action="cajafuerte.php" method="POST">
-            <input type="number" name="campoPass" placeholder="Introduzca una combinación..."/>
-            <input type="button" value="Comprobar"/>
+            <input type="number" name="campoNumSecreto" placeholder="Introduzca un número..."/>
+            <input type="submit" value="Comprobar"/>
         </form>
     </body>
 </html>
