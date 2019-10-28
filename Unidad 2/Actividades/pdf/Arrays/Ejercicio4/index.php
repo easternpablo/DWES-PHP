e4b732e96b325bdce73603dd09ddd8240c1f0294
easternpablo <easternpablo@gmail.com>
2019-10-28 23:44:10 +0100

    commit

--- Unidad 2/Actividades/pdf/Arrays/Ejercicio4/index.php
+++ Unidad 2/Actividades/pdf/Arrays/Ejercicio4/index.php
@@ -10,7 +10,23 @@
             $juegos = array("Acción" => "GTA 5","Dishonored 2","Call of Duty",
                             "Aventura" => "Assassins Creed","Tomb Raider","The Last of Us",
                             "Deporte" => "Fifa","Pes","Moto G");
-        
+            
         ?>
+        <table border="2px">
+            <tr>
+                <?php 
+                    foreach($juegos as $key => $valor){
+                        echo "<th colspan='3'> $key </th>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    foreach($juegos as $valor){
+                        echo "<td> $valor </td>";
+                    }
+                ?>
+            </tr>
+        </table>
     </body>
 </html>
