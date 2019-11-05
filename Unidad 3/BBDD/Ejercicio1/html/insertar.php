3cc0538e131233fecb7735080be42ef8e931d9f2
easternpablo <easternpablo@gmail.com>
2019-11-05 12:56:27 +0100

    commit

--- Unidad 3/BBDD/Ejercicio1/html/insertar.php
+++ Unidad 3/BBDD/Ejercicio1/html/insertar.php
@@ -9,19 +9,19 @@
     $errores = Array();
         
     if(empty($modelo)){
-        $errores['modelo'] = "Error en el campo modelo";
+        $errores['modelo'] = "Error en el campo modelo.<br>";
     }
     
     if(empty($marca)){
-        $errores['marca'] = "Error en el campo marca";
+        $errores['marca'] = "Error en el campo marca.<br>";
     }
     
     if(empty($precio)){
-        $errores['precio'] = "Error en el campo precio";
+        $errores['precio'] = "Error en el campo precio.<br>";
     }
     
     if(empty($stock)){
-        $errores['stock'] = "Error en el campo stock";
+        $errores['stock'] = "Error en el campo stock.<br>";
     }
     
     if(count($errores)==0){
@@ -33,5 +33,6 @@
             header("Location: listar_coches.php");
         }else{
             echo "Error: ". mysqli_error($conexion);
-        } 
+        }
+        
     }
