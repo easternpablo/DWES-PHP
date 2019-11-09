bd759450e6a9e5c2624e826ed4f008e01224eb84
easternpablo <easternpablo@gmail.com>
2019-11-09 19:46:11 +0100

    commit

---
+++ Unidad 3/BBDD/Ejercicio2/html/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login - Concesionario</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="../css/estilos.css">
+    </head>
+    <body>
+        
+        <?php require_once '../includes/navbar.html'; ?>
+        
+        <div class="seccion-principal">
+            <div class="row">
+                <div class="col-lg-6">
+                    <form class="formulario-login">
+                        <fieldset>
+                            <legend>Iniciar Sesión</legend>
+                            <div class="form-group">
+                                <label>Email</label><br>
+                                <input type="email" placeholder="Enter email" class="form-control" name="email" required="true"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Contraseña</label><br>
+                                <input type="password" placeholder="Enter contraseña" class="form-control" name="password" required="true"/>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+                            </div>
+                        </fieldset>
+                    </form> 
+                </div>
+                <div class="col-lg-6">
+                    <form class="formulario-registro">
+                        <fieldset>
+                            <legend>Registro Usuario</legend>
+                            <div class="form-group">
+                                <label>Email</label><br>
+                                <input type="email" placeholder="Enter email" class="form-control" name="email" required="true"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Contraseña</label><br>
+                                <input type="password" placeholder="Enter contraseña" class="form-control" name="password1" required="true"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Repita Contraseña</label><br>
+                                <input type="password" placeholder="Repeat contraseña" class="form-control" name="password2" required="true"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Nombre</label><br>
+                                <input type="text" placeholder="Enter your name" class="form-control" name="nombre"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Apellidos</label><br>
+                                <input type="text" placeholder="Enter your surname" class="form-control" name="apellidos"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Edad</label><br>
+                                <input type="number" placeholder="Enter your age" class="form-control" name="edad"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Dirección</label><br>
+                                <input type="text" placeholder="Enter your address" class="form-control" name="direccion" required="true"/>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Registrar</button>
+                                <button type="reset" class="btn btn-primary">Resetear</button>
+                            </div>
+                        </fieldset>
+                    </form> 
+                </div>
+            </div>           
+        </div>
+        
+        <?php require_once '../includes/footer.html'; ?>
+        
+    </body>
+</html>
