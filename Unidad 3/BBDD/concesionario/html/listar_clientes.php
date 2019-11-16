0cc8a15fcd409110fc538dfd82c5a308be712d6b
easternpablo <easternpablo@gmail.com>
2019-11-16 20:21:57 +0100

    commit

---
+++ Unidad 3/BBDD/concesionario/html/listar_clientes.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Listado de clientes</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="../css/estilos.css">
+    </head>
+    <body>
+        
+        <?php
+            
+            require_once 'conexion.php';
+            
+            require_once '../includes/navbar.html';
+            
+            $sql = " SELECT * FROM Clientes ";
+            $result = mysqli_query($conexion,$sql);
+            
+            if(mysqli_num_rows($result)>0){ ?>
+                <div class="seccion-principal">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Identificador</th>
+                                <th>Nombre</th>
+                                <th>Ciudad</th>
+                                <th>Gastado</th>
+                                <th>Acción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            
+                                while($fila = mysqli_fetch_assoc($result)){ ?>
+                                    <tr>
+                                        <td><?= $fila['id'] ?></td>
+                                        <td><?= $fila['nombre'] ?></td>
+                                        <td><?= $fila['ciudad'] ?></td>
+                                        <td><?= $fila['gastado'] ?></td>
+                                        <td>
+                                            <a href="#" class="fa fa-edit"></a>
+                                        </td>
+                                    </tr>
+                          <?php }
+            }else{
+                echo "0 registros";
+            }?>
+                        
+                        </tbody>
+                    </table>
+                </div>
+                                    
+        <?php require_once '../includes/footer.html'; ?>
+            
+    </body>
+</html>
